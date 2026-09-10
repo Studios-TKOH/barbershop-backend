277ccc1c7c98349edf1aad259955f28f005ea860
Juan Lavender <145859523+LavenderEdit@users.noreply.github.com>
2026-09-10 10:24:02 -0500

    feat: add health controller

---
+++ src/controllers/health.controller.ts
@@ -0,0 +1,6 @@
+import type { FastifyReply, FastifyRequest } from 'fastify';
+import { getHealth } from '../services/health.service.js';
+
+export async function healthController(_request: FastifyRequest, reply: FastifyReply) {
+  return reply.code(200).send(getHealth());
+}
